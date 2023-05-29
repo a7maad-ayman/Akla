0e19f82b7216d8f4320d99cbc1245c748c33f159
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2023-05-29 19:23:39 +0300

    Build : Add TabBar as a key window  🪟 (#3)
    * feat: Update compatibility version, change build number, and organize file groups
    
    * feat: Integrate SwiftGen for code generation
    
    * feat: Integrate SwiftGen for code generation
    
    * feat: Add SwiftLint for code style enforcement
    
    * feat: Add factory package
    
    * feat(swiftgen): Remove dead code from swiftgen.yml
    
    * feat: Add TabBar as key window
    
    * refactor: none-dead code , App Structure , run script phase warning , refactor key window
    
    * refactor: none-dead code,adding switlint formater rules, fixing swiftGen, adding auto generated files to .gitignore
    
    * refactor: adding auto generated files to .gitignore

---
+++ Akla/Classes/SceneDelegate.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
+
+    }
+
+    // MARK: - Function to create the root tabBar
+    func createTabBar() -> UITabBarController {
+
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        UITabBar.appearance().backgroundColor = .systemGray6
+
+        let homeNC = createNavigationController(viewController: HomeViewController(),
+                                                title: "Home",
+                                                tabBarItem: UITabBarItem(
+                                                    title: "Home",
+                                                    image: UIImage(systemName: "homekit"), tag: 0))
+        tabBar.viewControllers = [homeNC]
+
+        return tabBar
+    }
+
+    // MARK: - Function to Embed in NavigationController in a ViewController
+    func createNavigationController(viewController:
+                                    UIViewController,
+                                    title: String,
+                                    tabBarItem:
+                                    UITabBarItem) -> UINavigationController {
+        let navigationController = viewController
+        navigationController.title = title
+        navigationController.tabBarItem = tabBarItem
+
+        return UINavigationController(rootViewController: navigationController)
+
+    }
+}
